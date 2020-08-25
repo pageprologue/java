b55ff8a9fc4eb681b3311ddad82c7e215e2d01c8
Chloe <page.prologue@gmail.com>
2020-08-26 02:03:32 +0900

    Init Project

---
+++ src/main/java/study/java/controller/Main.java
@@ -0,0 +1,15 @@
+package study.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main {
+
+    @GetMapping("/")
+    public String main() {
+
+        return "index";
+    }
+    
+}
